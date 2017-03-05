5ce28f2662b72b7f1f480c5b81de532507e239b9
MrFlyingChip <kv_l@ukr.net>
2017-03-05 12:57:18 +0200

    Project created

---
+++ Barbershop/Queue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbershop
+{
+    class Queue
+    {
+        private List<QueuePlace> queue;
+        private int elementsCount;
+    }
+}
