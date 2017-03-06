94d2ed48a0bd8f402a48095e8ee27d25d4c50d10
MrFlyingChip <kv_l@ukr.net>
2017-03-06 07:46:49 +0200

    Queue add

--- Barbershop/Queue.cs
+++ Barbershop/Queue.cs
@@ -10,5 +10,37 @@ namespace Barbershop
     {
         private List<QueuePlace> queue;
         private int elementsCount;
+
+        private int timeNewClient;
+        private int currentTimeNewClient;
+
+        public Queue(int queueSize, int timeNewClient)
+        {
+            elementsCount = queueSize;
+            queue = new List<QueuePlace>();
+            this.timeNewClient = timeNewClient;
+        }
+
+        public void PushNewClient(QueuePlace client)
+        {
+            if(queue.Count <= elementsCount)
+            {
+                queue.Add(client);
+            }
+        }
+
+        public QueuePlace PopClient()
+        {
+            QueuePlace firstClient = null;
+            if (queue.Count > 0)
+            {
+                firstClient = queue.First();
+                queue.Insert(0, queue.First());
+            }
+            return firstClient;
+        }
+
+        public 
+
     }
 }
